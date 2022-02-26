d68f7bf9aed070bda8bb4a603e16d4510c00f210
Yegor <alinta.kawakami+github@gmail.com>
2022-02-26 15:50:22 +0300

    custom-jquery

---
+++ Custom jQuery/customJQuery.js
@@ -0,0 +1,144 @@
+class JQuery {
+  constructor() {
+    this.elem;
+  }
+
+  element(elem) {
+    this.elem = elem;
+    return this;
+  }
+
+  addClass(classNames) {
+    const classes = classNames.split(" ");
+    this.elem.classList.add(...classes);
+    return this;
+  }
+
+  removeClass(classNames) {
+    const classes = classNames.split(" ");
+    this.elem.classList.remove(classes);
+    return this;
+  }
+
+  append(html) {
+    this.elem.insertAdjacentHTML("afterend", html);
+    return this;
+  }
+
+  remove(selector = "") {
+    if (selector) {
+      this.elem.querySelector(selector).remove();
+    } else {
+      this.elem.remove();
+    }
+    return this;
+  }
+
+  text(text) {
+    if (arguments.length === 0) {
+      console.log(this.elem.innerText);
+    }
+
+    if (arguments.length === 1) {
+      const newText = document.createTextNode(text);
+      this.elem.appendChild(newText);
+    }
+
+    return this;
+  }
+
+  attr(attributeName, value) {
+    if (arguments.length === 1) {
+      console.log(this.elem.getAttribute(attributeName));
+    }
+
+    if (arguments.length === 2) {
+      this.elem.setAttribute(attributeName, value);
+    }
+    return this;
+  }
+
+  children(selector = "") {
+    if (selector) {
+      console.log(this.elem.querySelector(selector));
+    } else {
+      for (let i = 0; i < this.elem.children.length; i++) {
+        console.log(this.elem.children[i]);
+      }
+    }
+    return this;
+  }
+
+  empty() {
+    while (this.elem.firstChild) {
+      this.elem.removeChild(this.elem.firstChild);
+    }
+    return this;
+  }
+
+  css(props, value) {
+    if (arguments.length === 1) {
+      const style = getComputedStyle(this.elem);
+
+      if (Array.isArray(props)) {
+        props.forEach((propName) => {
+          console.log(style[propName]);
+        });
+      } else {
+        console.log(style[props]);
+      }
+
+      if (typeof props === "object") {
+        Object.keys(props).forEach((key) => {
+          this.elem.style.setProperty(key, props[key]);
+        });
+      }
+    }
+
+    if (arguments.length === 2) {
+      this.elem.style.setProperty(props, value);
+    }
+
+    return this;
+  }
+
+  click(handler) {
+    if (arguments.length === 0) {
+      this.elem.trigger("click");
+    }
+
+    if (arguments.length === 1) {
+      this.elem.addEventListener("click", handler);
+    }
+
+    return this;
+  }
+}
+
+const elem2 = document.querySelector(".element-2");
+const elem4 = document.querySelector(".element-4");
+const elem5 = document.querySelector(".element-5");
+const elem6 = document.querySelector(".element-6");
+
+const jquery = new JQuery();
+jquery
+  .element(elem2)
+  .addClass("some-class another-class")
+  .removeClass("some-class")
+  .append("<p class='jquery-elem'>JQUERY</p>")
+  .attr("id")
+  .attr("id", "new-id")
+  .children()
+  .css({ color: "green", "font-weight": "bold" });
+
+jquery
+  .element(elem4)
+  .text("Random text")
+  .text()
+  .css("background-color", "rgba(200, 200, 0)")
+  .click(() => {
+    alert("Hello!");
+  });
+
+jquery.element(elem5).empty();
+jquery.element(elem6).css(["color", "background-color"]);
