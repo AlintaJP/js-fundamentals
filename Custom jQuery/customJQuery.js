8a17a3b83784e020691d8b04afd4bbfe532c842c
Yegor <alinta.kawakami+github@gmail.com>
2022-03-10 17:09:58 +0500

    fixing comments

--- Custom jQuery/customJQuery.js
+++ Custom jQuery/customJQuery.js
@@ -7,23 +7,23 @@ class JQuery {
   }
 
   addClass(classNames) {
-    const classes = classNames.split(" ");
+    const classes = classNames.split(' ');
     this.elem.classList.add(...classes);
     return this;
   }
 
   removeClass(classNames) {
-    const classes = classNames.split(" ");
+    const classes = classNames.split(' ');
     this.elem.classList.remove(classes);
     return this;
   }
 
   append(html) {
-    this.elem.insertAdjacentHTML("afterend", html);
+    this.elem.insertAdjacentHTML('afterend', html);
     return this;
   }
 
-  remove(selector = "") {
+  remove(selector = '') {
     if (selector) {
       this.elem.querySelector(selector).remove();
     } else {
@@ -34,7 +34,7 @@ class JQuery {
 
   text(text) {
     if (arguments.length === 0) {
-      console.log(this.elem.innerText);
+      return this.elem.innerText;
     }
 
     if (arguments.length === 1) {
@@ -47,24 +47,26 @@ class JQuery {
 
   attr(attributeName, value) {
     if (arguments.length === 1) {
-      console.log(this.elem.getAttribute(attributeName));
+      return this.elem.getAttribute(attributeName);
     }
 
     if (arguments.length === 2) {
       this.elem.setAttribute(attributeName, value);
     }
+
     return this;
   }
 
-  children(selector = "") {
+  children(selector = '') {
     if (selector) {
-      console.log(this.elem.querySelector(selector));
+      return this.elem.querySelector(selector);
     } else {
+      let childrenArray = [];
       for (let i = 0; i < this.elem.children.length; i++) {
-        console.log(this.elem.children[i]);
+        childrenArray.push(this.elem.children[i]);
       }
+      return childrenArray;
     }
-    return this;
   }
 
   empty() {
@@ -78,18 +80,21 @@ class JQuery {
     if (arguments.length === 1) {
       const style = getComputedStyle(this.elem);
 
-      if (Array.isArray(props)) {
-        props.forEach((propName) => {
-          console.log(style[propName]);
-        });
-      } else {
-        console.log(style[props]);
-      }
-
-      if (typeof props === "object") {
+      if (typeof props === 'object') {
         Object.keys(props).forEach((key) => {
           this.elem.style.setProperty(key, props[key]);
         });
+      }
+
+      if (Array.isArray(props)) {
+        const propsObj = {};
+        props.forEach((propName) => {
+          propsObj[propName] = style[propName];
+        });
+        console.log(propsObj);
+        return propsObj;
+      } else {
+        return style[props];
       }
     }
 
@@ -102,36 +107,13 @@ class JQuery {
 
   click(handler) {
     if (arguments.length === 0) {
-      this.elem.trigger("click");
+      this.elem.trigger('click');
     }
 
     if (arguments.length === 1) {
-      this.elem.addEventListener("click", handler);
+      this.elem.addEventListener('click', handler);
     }
 
     return this;
   }
 }
-
-const jquery = new JQuery();
-jquery
-  .query(".element-2")
-  .addClass("some-class another-class")
-  .removeClass("some-class")
-  .append("<p class='jquery-elem'>JQUERY</p>")
-  .attr("id")
-  .attr("id", "new-id")
-  .children()
-  .css({ color: "green", "font-weight": "bold" });
-
-jquery
-  .query(".element-4")
-  .text("Random text")
-  .text()
-  .css("background-color", "rgba(200, 200, 0)")
-  .click(() => {
-    alert("Hello!");
-  });
-
-jquery.query(".element-5").empty();
-jquery.query(".element-6").css(["color", "background-color"]);
